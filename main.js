dd6d60a1d29eed3e8a6757c43eaf850c1ef95b7e
kyle0910 <46923397+kyle0910@users.noreply.github.com>
2019-09-23 07:45:39 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function insert(num){
+  $('.display').val($('.display').val() + num)
+}
+
+function equal(){
+  $('.display').val(eval($('.display').val()))
+}
+
+function clr(){
+  $('.display').val('');
+}
+
+function del(){
+  value = $('.display').val()
+  $('.display').val(value.substring(0,value.length - 1))
+}
